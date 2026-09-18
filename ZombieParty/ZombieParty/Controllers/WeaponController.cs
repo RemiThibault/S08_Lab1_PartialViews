2e768dcd62db7983045f5f9aab4769ba3cfd8cca
RemiThibault <6291762@cegepmontpetit.ca>
2026-09-18 09:36:46 -0400

    FCT Upsert Weapon Ajout vue partielle _UpsertButtons
    Ajout de la vue partielle _Upsert utilisée dans Weapon/Upsert et de la vue Weapon/upsert

--- ZombieParty/ZombieParty/Controllers/WeaponController.cs
+++ ZombieParty/ZombieParty/Controllers/WeaponController.cs
@@ -20,20 +20,30 @@ namespace ZombieParty.Controllers
             return View(weapons);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            if (id == null || id == 0) return View(new Weapon());
+            else return View(_baseDonnees.Weapons.Find(id));
         }
 
         [HttpPost]
-        public IActionResult Create(Weapon weapon)
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Weapon weapon)
         {
             if (ModelState.IsValid)
             {
-                // Ajouter à la BD
-                _baseDonnees.Weapons.Add(weapon);
-                TempData["Success"] = $"{weapon.Name} weapon added";
-
+                if (weapon.WeaponId == 0)
+                {
+                    // Ajouter à la BD
+                    _baseDonnees.Weapons.Add(weapon);
+                    TempData["Success"] = $"{weapon.Name} weapon added";
+                }
+                else
+                {
+                    // Update
+                    _baseDonnees.Weapons.Update(weapon);
+                    TempData["success"] = $"{weapon.Name} weapon updated";
+                }
                 _baseDonnees.SaveChanges();
 
                 return this.RedirectToAction("Index");
